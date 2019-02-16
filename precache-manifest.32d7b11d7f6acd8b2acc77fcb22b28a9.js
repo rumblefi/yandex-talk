7f32a2e9a090538d7a6a586f4f05f9a878b81c65
Yuriy Bahur <yuriybahur1@gmail.com>
2019-02-16 04:08:23 +0200

    Updates

---
+++ precache-manifest.32d7b11d7f6acd8b2acc77fcb22b28a9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "24577d08baaa51b38163da174948b26f",
+    "url": "/static/media/HelveticaNeueCyr-Roman.24577d08.woff2"
+  },
+  {
+    "revision": "f581c9d6a14cb318cebe",
+    "url": "/static/css/main.a3b868b3.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d6628012f553d48f4776424ba56ba747",
+    "url": "/static/media/HelveticaNeueCyr-Bold.d6628012.woff"
+  },
+  {
+    "revision": "f272876e66648fb13549",
+    "url": "/static/js/2.f272876e.chunk.js"
+  },
+  {
+    "revision": "774e34d9dd8731311de88196d86c068d",
+    "url": "/static/media/logo.774e34d9.svg"
+  },
+  {
+    "revision": "f581c9d6a14cb318cebe",
+    "url": "/static/js/main.f581c9d6.chunk.js"
+  },
+  {
+    "revision": "713814ead540f7770e0fa7f45c19fe7c",
+    "url": "/static/media/HelveticaNeueCyr-Medium.713814ea.woff2"
+  },
+  {
+    "revision": "c9703f00461cd358d9d7394f6b95c395",
+    "url": "/static/media/HelveticaNeueCyr-Roman.c9703f00.woff"
+  },
+  {
+    "revision": "176ce3c24f825b034daa73ca15461018",
+    "url": "/static/media/HelveticaNeueCyr-Medium.176ce3c2.woff"
+  },
+  {
+    "revision": "aa08ca62d2ed0ed946f9106ce77cfad1",
+    "url": "/static/media/HelveticaNeueCyr-Bold.aa08ca62.woff2"
+  },
+  {
+    "revision": "f272876e66648fb13549",
+    "url": "/static/css/2.0aec5aca.chunk.css"
+  },
+  {
+    "revision": "b89403e5cc84c7771f94698640a73dda",
+    "url": "/index.html"
+  }
+];
