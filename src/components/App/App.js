d328311f74fb63593262fe458904e0c7d890c0da
Yuriy Bahur <yuriybahur1@gmail.com>
2019-02-17 16:51:24 +0200

    Говноанимация

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,29 @@ import Header from '../Header/Header'
 import 'normalize.css/normalize.css'
 import '../../index.scss'
 import './App.scss'
-import {Router} from "@reach/router";
+import {Router, Link, Location} from "@reach/router";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 import SchedulerComponent from '../SchedulerComponent/SchedulerComponent'
 import Meeting from '../Meeting/Meeting'
 import NotFound from '../NotFound/NotFound'
+
+const FadeTransitionRouter = props => (
+    <Location>
+        {({location}) => (
+            <TransitionGroup className="transition-group">
+                <CSSTransition key={location.key} classNames="fade" timeout={100}>
+                    {/* the only difference between a router animation and
+                any other animation is that you have to pass the
+                location to the router so the old screen renders
+                the "old location" */}
+                    <Router location={location} className="router">
+                        {props.children}
+                    </Router>
+                </CSSTransition>
+            </TransitionGroup>
+        )}
+    </Location>
+);
 
 export default class App extends React.Component {
 
@@ -17,11 +36,11 @@ export default class App extends React.Component {
                     <Header/>
                 </div>
                 <div className="app__main">
-                    <Router>
+                    <FadeTransitionRouter>
                         <SchedulerComponent path="/"/>
                         <Meeting path="meeting"/>
-                        <NotFound default />
-                    </Router>
+                        <NotFound default/>
+                    </FadeTransitionRouter>
                 </div>
 
             </div>
